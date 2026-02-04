19036947490ac8eef3a08610e6bcf841d5a05904
Tim Toresson <tim.toresson@knowe.se>
2026-02-04 17:39:45 +0100

    feat(domain): add validation logic that aligns with swedish accounting standards.

--- src/ReceiptReader.Domain/Entities/ReceiptTaxLine.cs
+++ src/ReceiptReader.Domain/Entities/ReceiptTaxLine.cs
@@ -16,6 +16,41 @@ namespace ReceiptReader.Domain.Entities
         public Guid? ReceiptInfoId { get; private set; }
         public ReceiptInfo? ReceiptInfo { get; private set; }
 
+        public bool IsValid => !GetValidationErrors().Any();
+
+        public IEnumerable<string> GetValidationErrors()
+        {
+            decimal Round(decimal value)
+            {
+                return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+            }
+
+            if (Percentage < 0.0m || Percentage > 1.0m)
+            {
+                yield return "Tax percentage must be assigned as a factor (e.g. 0.25).";
+            }
+            if (Math.Abs(TaxAmount) > Math.Abs(GrossAmount))
+            {
+                yield return "Absolute tax amount cannot be greater than absolute gross amount.";
+            }
+            if (Math.Abs(NetAmount) > Math.Abs(GrossAmount))
+            {
+                yield return "Absolute net amount cannot be greater than absolute gross amount.";
+            }
+            if (Round(NetAmount) != Round(GrossAmount - TaxAmount))
+            {
+                yield return $"Net amount {NetAmount} must be equal to gross {GrossAmount} minus tax {TaxAmount}.";
+            }
+            if (Round(TaxAmount) != Round(NetAmount * Percentage))
+            {
+                yield return $"Tax amount {TaxAmount} must be equal to net {NetAmount} times percentage {Percentage}.";
+            }
+            if (ReceiptInfoId == null || ReceiptInfoId == Guid.Empty)
+            {
+                yield return "Tax line must be linked to a receipt";
+            }
+        }
+
         public void LinkToReceipt(Guid receiptInfoId)
         {
             if (receiptInfoId == Guid.Empty)
